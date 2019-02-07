92dc1cefc18ce47d4792c0e982bd20883a638493
Simon Chemouil <simon.chemouil@lambdacube.fr>
2019-02-07 19:25:10 +0100

    Remove Serialize trait bound on Gauge return types (refactoring gone wrong); example in demo-nightly externally measuring non-blocking functions

--- demos/demo-nightly/src/baz.rs
+++ demos/demo-nightly/src/baz.rs
@@ -55,6 +55,22 @@ impl Baz {
         }
     }
 
+    #[measure([ResponseTime])]
+    pub fn bazium(&self, should_fail: bool) -> impl std::future::Future<Output = Result<(), &'static str>> {
+        async move {
+            let delay = std::time::Duration::from_millis(rand::random::<u64>() % 2000);
+
+            let when = std::time::Instant::now() + delay;
+            tokio::await!(tokio::timer::Delay::new(when)).map_err(|_| "Tokio timer error")?;
+            if !should_fail {
+                println!("baz !");
+                Ok(())
+            } else {
+                Err("I failed!")
+            }
+        }
+    }
+
     // This is not measured either
     pub fn bur() {
         println!("bur");
--- metered/src/common/in_flight.rs
+++ metered/src/common/in_flight.rs
@@ -19,7 +19,7 @@ use serde::Serialize;
 #[derive(Clone, Default, Debug, Serialize)]
 pub struct InFlight<G: Gauge = AtomicInt<u64>>(G);
 
-impl<G: Gauge, R: Serialize> Metric<R> for InFlight<G> {}
+impl<G: Gauge, R> Metric<R> for InFlight<G> {}
 
 impl<G: Gauge> Enter for InFlight<G> {
     type E = ();
